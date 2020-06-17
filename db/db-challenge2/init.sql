ca426156e8216dc57fcb617d9b69f06f08523ff1
kaneyan0607 <kaneyan0607@gmail.com>
2020-06-18 01:35:39 +0900

    Fix:users.self_introductionの長さを1000に変更。 物理名を一部修正。 データベース定義書にてtext型からvarcharに変更。 Remove:chat_room_entriesのchat_room_entry_idカラムを削除。

--- db/db-challenge2/init.sql
+++ db/db-challenge2/init.sql
@@ -1,12 +1,12 @@
 CREATE TABLE `users` (
       `user_id` int(11) NOT NULL AUTO_INCREMENT,
       `name` varchar(100) NOT NULL,
-      `self_introduction` varchar(100),
+      `self_introduction` varchar(1000),
       `cell_phone_number` varchar(13),
       `work_phone_number` varchar(13),
       `email` varchar(100) NOT NULL,
       `password` varchar(100) NOT NULL,
-      `deleted_flag` tinyint(1) DEFAULT 0 NOT NULL,
+      `is_deleted` tinyint(1) DEFAULT 0 NOT NULL,
       `created_at` datetime NOT NULL,
       `updated_at` datetime NOT NULL,
       PRIMARY KEY (`user_id`)
@@ -15,24 +15,23 @@ CREATE TABLE `users` (
 CREATE TABLE `chat_rooms` (
       `chat_room_id` int(11) NOT NULL AUTO_INCREMENT,
       `overview` varchar(1000),
-      `suppression_flag` tinyint(1) DEFAULT 0 NOT NULL,
-      `direct_chat_flag` tinyint(1) DEFAULT 0 NOT NULL,
-      `deleted_flag` tinyint(1) DEFAULT 0 NOT NULL,
-      `create_user_id` int(11) NOT NULL,
+      `is_suppression` tinyint(1) DEFAULT 0 NOT NULL,
+      `is_direct_chat` tinyint(1) DEFAULT 0 NOT NULL,
+      `is_deleted` tinyint(1) DEFAULT 0 NOT NULL,
+      `created_user_id` int(11) NOT NULL,
       `updated_user_id` int(11) NOT NULL,
       `created_at` datetime NOT NULL,
       `updated_at` datetime NOT NULL,
       PRIMARY KEY (`chat_room_id`),
-      FOREIGN KEY (`create_user_id`) REFERENCES users(`user_id`),
+      FOREIGN KEY (`created_user_id`) REFERENCES users(`user_id`),
       FOREIGN KEY (`updated_user_id`) REFERENCES users(`user_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
 
 CREATE TABLE `chat_room_entries` (
-      `chat_room_entry_id` int(11) NOT NULL AUTO_INCREMENT,
       `user_id` int(11) NOT NULL,
       `chat_room_id` int(11) NOT NULL,
-      `participate_at` datetime NOT NULL,
-      PRIMARY KEY (`chat_room_entry_id`),
+      `participated_at` datetime NOT NULL,
+      PRIMARY KEY (`user_id`,`chat_room_id`),
       FOREIGN KEY (`user_id`) REFERENCES users(`user_id`),
       FOREIGN KEY (`chat_room_id`) REFERENCES chat_rooms(`chat_room_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
@@ -42,14 +41,14 @@ CREATE TABLE `posts` (
       `message` varchar(1000),
       `attachment_name` varchar(100),
       `chat_room_id` int(11) NOT NULL,
-      `deleted_flag` tinyint(1) DEFAULT 0 NOT NULL,
-      `create_user_id` int(11) NOT NULL,
+      `is_deleted` tinyint(1) DEFAULT 0 NOT NULL,
+      `created_user_id` int(11) NOT NULL,
       `updated_user_id` int(11) NOT NULL,
       `created_at` datetime NOT NULL,
       `updated_at` datetime NOT NULL,
       PRIMARY KEY (`posts_id`),
       FOREIGN KEY (`chat_room_id`) REFERENCES chat_rooms(`chat_room_id`),
-      FOREIGN KEY (`create_user_id`) REFERENCES users(`user_id`),
+      FOREIGN KEY (`created_user_id`) REFERENCES users(`user_id`),
       FOREIGN KEY (`updated_user_id`) REFERENCES users(`user_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
 
@@ -58,15 +57,15 @@ CREATE TABLE `tasks` (
       `task_message` varchar(1000) NOT NULL,
       `responsible_id` int(11) NOT NULL,
       `deadline` datetime,
-      `complete_flag` tinyint(1) DEFAULT 0 NOT NULL,
+      `is_complete` tinyint(1) DEFAULT 0 NOT NULL,
       `chat_room_id` int(11) NOT NULL,
-      `deleted_flag` tinyint(1) DEFAULT 0 NOT NULL,
-      `create_user_id` int(11) NOT NULL,
+      `is_deleted` tinyint(1) DEFAULT 0 NOT NULL,
+      `created_user_id` int(11) NOT NULL,
       `updated_user_id` int(11) NOT NULL,
       `created_at` datetime NOT NULL,
       `updated_at` datetime NOT NULL,
       PRIMARY KEY (`task_id`),
       FOREIGN KEY (`chat_room_id`) REFERENCES chat_rooms(`chat_room_id`),
-      FOREIGN KEY (`create_user_id`) REFERENCES users(`user_id`),
+      FOREIGN KEY (`created_user_id`) REFERENCES users(`user_id`),
       FOREIGN KEY (`updated_user_id`) REFERENCES users(`user_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
