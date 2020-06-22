d4a71880ccb81139c13889bc4744e71b36aaae75
kaneyan0607 <kaneyan0607@gmail.com>
2020-06-23 00:48:04 +0900

    fix: 削除されているユーザの投稿を含まずに最新の投稿を取得する条件を修正 スクショを差し替え

--- db/db-challenge3/db-010/db_010.sql
+++ db/db-challenge3/db-010/db_010.sql
@@ -4,11 +4,13 @@ JOIN users
 ON posts.created_user_id = users.user_id
 JOIN chat_rooms
 ON posts.chat_room_id = chat_rooms.chat_room_id
-WHERE users.is_deleted = 0
-AND (posts.chat_room_id, posts.created_at) IN(
+WHERE (posts.chat_room_id, posts.created_at) IN(
     SELECT posts.chat_room_id, MAX(posts.created_at)
     FROM posts
+    JOIN users
+    ON posts.created_user_id = users.user_id
     WHERE posts.is_deleted = 0
+    AND users.is_deleted = 0
     GROUP BY posts.chat_room_id
     )
 ORDER BY chat_rooms.chat_room_id ASC;
