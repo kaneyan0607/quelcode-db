5ee759deb1cb827b7ff17e35528b8a9c10caab6c
kaneyan0607 <kaneyan0607@gmail.com>
2020-06-23 00:39:30 +0900

    add:削除されているユーザー、削除されているチャットルームは表示しない条件を追記。 スクショを差し替え

--- db/db-challenge3/db-008/db_008.sql
+++ db/db-challenge3/db-008/db_008.sql
@@ -4,4 +4,6 @@ JOIN users
 ON users.user_id = chat_room_entries.user_id
 JOIN chat_rooms
 ON chat_rooms.chat_room_id = chat_room_entries.chat_room_id
+WHERE users.is_deleted = 0
+AND chat_rooms.is_deleted = 0
 ORDER BY chat_room_entries.participated_at;
