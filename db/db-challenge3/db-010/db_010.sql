2724de5913fc1b69286fe64151142f67184eddbe
kaneyan0607 <kaneyan0607@gmail.com>
2020-06-23 00:41:16 +0900

    Fix:ユーザの投稿を含まずに最新の投稿を取得するよう修正。 投稿日時のみを抽出条件とせず、chat_room_idもINの条件に含めた。 スクショの差し替え

--- db/db-challenge3/db-010/db_010.sql
+++ db/db-challenge3/db-010/db_010.sql
@@ -5,8 +5,8 @@ ON posts.created_user_id = users.user_id
 JOIN chat_rooms
 ON posts.chat_room_id = chat_rooms.chat_room_id
 WHERE users.is_deleted = 0
-AND posts.created_at IN(
-    SELECT MAX(posts.created_at)
+AND (posts.chat_room_id, posts.created_at) IN(
+    SELECT posts.chat_room_id, MAX(posts.created_at)
     FROM posts
     WHERE posts.is_deleted = 0
     GROUP BY posts.chat_room_id
