96a031aafea326e7e6bf3ad1fb16df1febd1311c
kaneyan0607 <kaneyan0607@gmail.com>
2020-06-24 01:30:15 +0900

    remove:不要なテーブル名の記述を削除 スクショを差し替え

--- db/db-challenge3/db-013/db_013.sql
+++ db/db-challenge3/db-013/db_013.sql
@@ -1,6 +1,6 @@
 BEGIN;
 UPDATE users
-SET users.is_deleted = 1, users.updated_at = NOW()
+SET is_deleted = 1, updated_at = NOW()
 WHERE cell_phone_number IS NULL
 AND work_phone_number IS NULL;
 COMMIT;
