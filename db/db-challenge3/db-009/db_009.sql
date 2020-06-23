59b73a3c0b046f079118faab30ab1a557c060102
kaneyan0607 <kaneyan0607@gmail.com>
2020-06-23 16:22:24 +0900

    fix:サブクエリではなくjoinを使って絞り込む用修正 スクショの画像差し替え

--- db/db-challenge3/db-009/db_009.sql
+++ db/db-challenge3/db-009/db_009.sql
@@ -1,11 +1,10 @@
 SELECT chat_rooms.chat_room_name, COUNT(posts.chat_room_id) AS '投稿数'
-FROM chat_rooms
-JOIN posts
+FROM posts
+JOIN chat_rooms
 ON chat_rooms.chat_room_id = posts.chat_room_id
+JOIN users
+ON posts.created_user_id = users.user_id
 WHERE posts.is_deleted = 0
-AND posts.created_user_id IN (
-    SELECT users.user_id
-    FROM users
-    WHERE users.is_deleted = 0)
+AND users.is_deleted = 0
 GROUP BY posts.chat_room_id
 ORDER BY COUNT(posts.chat_room_id) DESC;
