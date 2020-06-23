9e190dfc9720f380ffcd12663754599d67f1b114
kaneyan0607 <kaneyan0607@gmail.com>
2020-06-24 01:27:34 +0900

    remove:不要なテーブル名の記述を削除 スクショを差し替え

--- db/db-challenge3/db-011/db_011.sql
+++ db/db-challenge3/db-011/db_011.sql
@@ -1,6 +1,6 @@
 BEGIN;
 UPDATE tasks
 SET is_completed = 1, updated_user_id = 1, updated_at = NOW()
-WHERE tasks.created_at BETWEEN '2020/4/26 09:00' AND '2020/4/26 11:30'
-AND tasks.is_deleted = 0
+WHERE created_at BETWEEN '2020/4/26 09:00' AND '2020/4/26 11:30'
+AND is_deleted = 0;
 COMMIT;
